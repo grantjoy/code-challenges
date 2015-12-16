69b888e9ab3ed35a5d04829abc5e07c3e18d91bc
Grant Joy <grant.joy@distilnetworks.com>
2015-12-15 21:43:28 -0800

    New challenges!

---
+++ detecting_cycles/detect.js
@@ -0,0 +1,12 @@
+function find_cycle(data) {
+  estimated_cycle = []
+  data.forEach();
+}
+
+var fs  = require("fs");
+fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+  if (line != "") {
+    line.split(" ").forEach(find_cycle(data));
+    console.log(result);
+  }
+);
